d81961f26285d0a964fbdbc1be7664c0bf33d3a6
Gilson Alencar <gilson.alencar94@gmail.com>
2022-05-30 16:07:19 -0300

    ✨ feat(migrations): create matches migration

---
+++ app/backend/src/database/migrations/20220530185540-create-matches.js
@@ -0,0 +1,44 @@
+"use strict";
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable("matches", {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      home_team: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: {
+          model: "teams",
+          key: "id",
+        },
+      },
+      home_team_goals: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+      },
+      away_team: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: {
+          model: "teams",
+          key: "id",
+        },
+      },
+      away_team_goals: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+      },
+      in_progess: {
+        type: Sequelize.BOOLEAN,
+        allowNull: false,
+      },
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable("matches");
+  },
+};
