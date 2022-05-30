a5848e239f57a6a38d231660891a7bd72c4ef46f
Gilson Alencar <gilson.alencar94@gmail.com>
2022-05-30 16:35:18 -0300

    ✨ feat(models): create "User" model

---
+++ app/backend/src/database/models/User.ts
@@ -0,0 +1,40 @@
+import { DataTypes, Model } from 'sequelize';
+import db from '.';
+
+class User extends Model {
+  // public <campo>!: <tipo>;
+}
+
+User.init(
+  {
+    id: {
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+      type: DataTypes.INTEGER,
+    },
+    username: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    role: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  },
+  {
+    underscored: true,
+    sequelize: db,
+    timestamps: false,
+  },
+);
+
+export default User;
