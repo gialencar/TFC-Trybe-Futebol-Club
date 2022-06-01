d8b302b2a228c61a7b0048b3d3860ebb76cb144d
Gilson Alencar <gilson.alencar94@gmail.com>
2022-06-01 17:53:23 -0300

    ✨ feat(users): create validate use case

---
+++ app/backend/src/users/useCases/Validate/ValidateUseCase.ts
@@ -0,0 +1,22 @@
+import { readFileSync } from 'fs';
+import { JwtPayload, verify } from 'jsonwebtoken';
+import { resolve } from 'path';
+import User from '../../../database/models/User';
+
+export default class ValidateUseCase {
+  execute = async (token: string) => {
+    const jwtSecret = readFileSync(
+      resolve(__dirname, '../../../../jwt.evaluation.key'),
+      { encoding: 'utf-8' },
+    );
+    const decoded = <JwtPayload>verify(token, jwtSecret);
+    console.log({ decoded });
+
+    const userExists = await User.findOne({
+      where: { email: decoded },
+    });
+    if (!userExists) return;
+
+    return userExists.role;
+  };
+}
