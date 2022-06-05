6c587211f3262d3281a4930a61cdb49b3f222d68
Gilson Alencar <gilson.alencar94@gmail.com>
2022-06-04 21:15:46 -0300

    ✨ feat(teams): create ListTeamsUseCase

---
+++ app/backend/src/teams/useCases/ListTeams/ListTeamsUseCase.ts
@@ -0,0 +1,12 @@
+import Team from '../../../database/models/Team';
+import ITeamDTO from './TeamDTO';
+
+export default class ListTeamsUseCase {
+  constructor(private teamModel = Team) {}
+
+  async execute(): Promise<ITeamDTO[]> {
+    const teams = await this.teamModel.findAll();
+
+    return JSON.parse(JSON.stringify(teams));
+  }
+}
