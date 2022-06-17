cb814b48526e60c4defebdb8a5acc4c604986d3f
Gilson Alencar <gilson.alencar94@gmail.com>
2022-06-17 15:27:31 -0300

    🧪 test(teams): add stubs

--- app/backend/src/tests/03-teams.test.ts
+++ app/backend/src/tests/03-teams.test.ts
@@ -1,6 +1,8 @@
 import * as chai from 'chai';
+import * as sinon from 'sinon';
 import { Response } from 'superagent';
 import { app } from '../app';
+import Team from '../database/models/Team';
 import validTeam from './mocks/validTeam';
 import validTeamList from './mocks/validTeamList';
 // @ts-ignore
@@ -12,8 +14,17 @@ const { expect } = chai;
 
 describe('Teams', () => {
   let chaiHttpResponse: Response;
+  let stub: sinon.SinonStub;
 
   describe('GET teams route', () => {
+    before(async () => {
+      stub = sinon.stub(Team, 'findAll').resolves(validTeamList as Team[]);
+    });
+
+    after(() => {
+      stub.restore();
+    });
+
     it('should return a 200 status code and the team list', async () => {
       chaiHttpResponse = await chai.request(app).get('/teams');
 
@@ -23,7 +34,15 @@ describe('Teams', () => {
   });
 
   describe('GET team route', () => {
+    afterEach(() => {
+      stub.restore();
+    });
+
     describe('Given the team does exist', () => {
+      before(async () => {
+        stub = sinon.stub(Team, 'findByPk').resolves(validTeam as Team);
+      });
+
       it('should return a 200 status code and the team', async () => {
         chaiHttpResponse = await chai.request(app).get('/teams/5');
 
@@ -33,6 +52,10 @@ describe('Teams', () => {
     });
 
     describe('Given the team does not exist', () => {
+      before(async () => {
+        stub = sinon.stub(Team, 'findByPk').resolves(null);
+      });
+
       it('should return a 404 status', async () => {
         chaiHttpResponse = await chai.request(app).get('/teams/42');
 
