880c242418c8ba71f1bd80d1d4f5357a4c7e2039
Gilson Alencar <gilson.alencar94@gmail.com>
2022-06-17 11:40:32 -0300

    🧪 test(teams): create failing tests
    for routes GET:/team and GET:/teams

--- app/backend/src/tests/03-teams.test.ts
+++ app/backend/src/tests/03-teams.test.ts
@@ -1,56 +1,42 @@
-import * as sinon from 'sinon';
 import * as chai from 'chai';
+import { Response } from 'superagent';
+import { app } from '../app';
+import validTeam from './mocks/validTeam';
+import validTeamList from './mocks/validTeamList';
 // @ts-ignore
 import chaiHttp = require('chai-http');
-
-import { app } from '../app';
-
-import { Response } from 'superagent';
 
 chai.use(chaiHttp);
 
 const { expect } = chai;
 
 describe('Teams', () => {
-  describe('Route GET:/teams', () => {
-    describe('Success case', () => {
-      let chaiHttpResponse: Response;
+  let chaiHttpResponse: Response;
 
-      it('Should respond with status code 200 and teams list on body', async () => {
-        chaiHttpResponse = await chai.request(app).get('/teams');
-        const teams = [
-          { id: 1, teamName: 'Avaí/Kindermann' },
-          { id: 2, teamName: 'Bahia' },
-          { id: 3, teamName: 'Botafogo' },
-          { id: 4, teamName: 'Corinthians' },
-          { id: 5, teamName: 'Cruzeiro' },
-          { id: 6, teamName: 'Ferroviária' },
-          { id: 7, teamName: 'Flamengo' },
-          { id: 8, teamName: 'Grêmio' },
-          { id: 9, teamName: 'Internacional' },
-          { id: 10, teamName: 'Minas Brasília' },
-          { id: 11, teamName: 'Napoli-SC' },
-          { id: 12, teamName: 'Palmeiras' },
-          { id: 13, teamName: 'Real Brasília' },
-          { id: 14, teamName: 'Santos' },
-          { id: 15, teamName: 'São José-SP' },
-          { id: 16, teamName: 'São Paulo' },
-        ];
-        expect(chaiHttpResponse.status).to.equal(200);
-        expect(chaiHttpResponse.body).to.deep.equal(teams);
-      });
+  describe('GET teams route', () => {
+    it('should return a 200 status code and the team list', async () => {
+      chaiHttpResponse = await chai.request(app).get('/teams');
+
+      expect(chaiHttpResponse.status).to.equal(200);
+      expect(chaiHttpResponse.body).to.deep.equal(validTeamList);
     });
   });
 
-  describe('Route GET:/teams/:id', () => {
-    describe('Success case', () => {
-      let chaiHttpResponse: Response;
-
-      it('Should respond with status code 200 and team on body', async () => {
+  describe('GET team route', () => {
+    describe('Given the team does exist', () => {
+      it('should return a 200 status code and the team', async () => {
         chaiHttpResponse = await chai.request(app).get('/teams/5');
-        const team = { id: 5, teamName: 'Cruzeiro' };
+
         expect(chaiHttpResponse.status).to.equal(200);
-        expect(chaiHttpResponse.body).to.deep.equal(team);
+        expect(chaiHttpResponse.body).to.deep.equal(validTeam);
+      });
+    });
+
+    describe('Given the team does not exist', () => {
+      it('should return a 404 status', async () => {
+        chaiHttpResponse = await chai.request(app).get('/teams/42');
+
+        expect(chaiHttpResponse.status).to.equal(404);
       });
     });
   });
