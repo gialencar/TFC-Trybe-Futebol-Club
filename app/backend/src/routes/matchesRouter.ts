736ad839d0ee39ba29c3b71dabdeeb07970ae51e
Gilson Alencar <gilson.alencar94@gmail.com>
2022-06-19 17:19:52 -0300

    ✨ feat(routes): add POST:/matches route

--- app/backend/src/routes/matchesRouter.ts
+++ app/backend/src/routes/matchesRouter.ts
@@ -1,10 +1,15 @@
 import { Router } from 'express';
+import createMatchCOntroller from '../matches/useCases/CreateMatch';
 import listMatchesController from '../matches/useCases/ListMatches';
 
 const matchesRouter = Router();
 
-matchesRouter.get('/', (req, res) => {
-  listMatchesController.handle(req, res);
-});
+matchesRouter
+  .get('/', (req, res) => {
+    listMatchesController.handle(req, res);
+  })
+  .post('/', (req, res) => {
+    createMatchCOntroller.handle(req, res);
+  });
 
 export default matchesRouter;
