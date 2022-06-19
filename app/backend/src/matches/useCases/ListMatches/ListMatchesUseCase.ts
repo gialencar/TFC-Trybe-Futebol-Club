6e90bd2a71c67a3cb7dbacf009174a37ab4d719b
Gilson Alencar <gilson.alencar94@gmail.com>
2022-06-18 23:05:23 -0300

    ✨ feat(matches): create ListMatchesUseCase

---
+++ app/backend/src/matches/useCases/ListMatches/ListMatchesUseCase.ts
@@ -0,0 +1,27 @@
+import * as queryString from 'query-string';
+import Match from '../../../database/models/Match';
+import Team from '../../../database/models/Team';
+
+export default class ListMatchesUseCase {
+  constructor(private matchModel = Match) {}
+
+  execute(inProgress?: string) {
+    let filter = {};
+
+    if (inProgress != null) {
+      filter = queryString.parse(`inProgress=${inProgress}`, {
+        parseBooleans: true,
+      });
+    }
+
+    const matches = this.matchModel.findAll({
+      where: filter, // empty object when func receives no param. effectively not filtering
+      include: [
+        { model: Team, as: 'teamHome', attributes: ['teamName'] },
+        { model: Team, as: 'teamAway', attributes: ['teamName'] },
+      ],
+    });
+
+    return matches;
+  }
+}
