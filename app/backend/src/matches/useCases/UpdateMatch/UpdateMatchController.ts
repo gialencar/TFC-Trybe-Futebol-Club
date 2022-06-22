47734cdedb3437c70ed91088983fdc71cc716e2e
Gilson Alencar <gilson.alencar94@gmail.com>
2022-06-22 15:58:43 -0300

    ✨ feat(matches): create UpdateMatchController

---
+++ app/backend/src/matches/useCases/UpdateMatch/UpdateMatchController.ts
@@ -0,0 +1,20 @@
+import { Request, Response } from 'express';
+import IController from '../../../common/interfaces/IController';
+import UpdateMatchUseCase from './UpdateMatchUseCase';
+
+export default class UpdateMatchController implements IController {
+  constructor(private updateMatchUseCase: UpdateMatchUseCase) {}
+
+  async handle(req: Request, res: Response): Promise<Response> {
+    const { id } = req.params;
+    const { homeTeamGoals, awayTeamGoals, finished } = req.body;
+
+    await this.updateMatchUseCase.execute(
+      +id,
+      { homeTeamGoals, awayTeamGoals },
+      finished,
+    );
+
+    return res.status(200).send();
+  }
+}
--- app/backend/src/matches/useCases/UpdateMatch/index.ts
+++ app/backend/src/matches/useCases/UpdateMatch/index.ts
@@ -1,3 +1,8 @@
+import UpdateMatchController from './UpdateMatchController';
 import UpdateMatchUseCase from './UpdateMatchUseCase';
 
 const updateMatchUseCase = new UpdateMatchUseCase();
+
+const updateMatchController = new UpdateMatchController(updateMatchUseCase);
+
+export default updateMatchController;
